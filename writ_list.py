0271a3d3ec64d8fd3c67ece3025e824506a23ef6
Raul-Pena <rnpena0805@gmail.com>
2020-04-14 22:50:04 -0500

    initial commit

---
+++ writ_list.py
@@ -0,0 +1,5 @@
+import writs as w
+class WritList:
+        def __init__(self):
+                self.wlist = []
+
---
+++ writs.py
@@ -0,0 +1,8 @@
+class Writs:
+        def __init__(self, style, wtype, trait, qual, Set):
+                self.style = style
+                self.wtype = wtype
+                self.trait = trait
+                self.qual = qual
+                self.Set = Set
+
